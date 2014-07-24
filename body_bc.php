d0e84eb8e7136450bbe7ce902db461b37ba45b4a
VanessaMaigne <vmaigne@gmail.com>
2014-07-24 16:18:49 +0200

    Clean indent files

--- body_bc.php
+++ body_bc.php
@@ -1,35 +1,35 @@
 <div id="pageWrapper">
     <div id="leftMenu" class="non-printable">
-	<div id="helpMenu"><img src="img/help_big_orange.png"></div>
-	<div class="leftMenu firstLeftMenu">
-	    <div id="submitCreateMap" class="orangeButton">Create map</div>
+        <div id="helpMenu"><img src="img/help_big_orange.png"></div>
+        <div class="leftMenu firstLeftMenu">
+            <div id="submitCreateMap" class="orangeButton">Create map</div>
         </div>
 
         <div class="leftMenu">
-	    <h2 title="Pick a projection">
+            <h2 title="Pick a projection">
                 <img src="img/1step.png">
                 PROJECTION
             </h2>
 
-	    <select id="projectionSelect">
-		<option value="EPSG:4087">World Equidistant Cylindrical</option>
-		<option value="EPSG:3857">Pseudo-Mercator</option>
-		<option value="EPSG:3408">EASE-Grid North</option>
-		<option value="EPSG:3409">EASE-Grid South</option>
-	    </select>
+            <select id="projectionSelect">
+                <option value="EPSG:4087">World Equidistant Cylindrical</option>
+                <option value="EPSG:3857">Pseudo-Mercator</option>
+                <option value="EPSG:3408">EASE-Grid North</option>
+                <option value="EPSG:3409">EASE-Grid South</option>
+            </select>
         </div>
 
-	<div class="leftMenu">
-       	    <h2 title="Choose your period">
-	        <img src="img/2step.png">
-	        AVERAGING PERIOD
-	    </h2>
+        <div class="leftMenu">
+            <h2 title="Choose your period">
+                <img src="img/2step.png">
+                AVERAGING PERIOD
+            </h2>
 
-	    <select id="periodSelect">
-		<option value="monthlymean">Monthly mean</option>
-		<option value="yearlymean">Yearly mean</option>
-		<option value="longterm">Long term mean</option>
-	    </select>
+            <select id="periodSelect">
+                <option value="monthlymean">Monthly mean</option>
+                <option value="yearlymean">Yearly mean</option>
+                <option value="longterm">Long term mean</option>
+            </select>
         </div>
 
         <div class="leftMenu">
@@ -39,21 +39,21 @@
             </h2>
 
             <div>
-		<input name="searchResource" placeholder="Filter..." size="10">
-	        <button id="btnResetSearchResource">&times;</button>
+                <input name="searchResource" placeholder="Filter..." size="10">
+                <button id="btnResetSearchResource">&times;</button>
 
                 <div id="resourceSelect"></div>
             </div>
         </div>
 
-	<div id="variables" class="leftMenu">
-        <h2 title="Intersection of variables available for all the selected resources">
-            <img src="img/4step.png">
-            VARIABLE
-        </h2>
+        <div id="variables" class="leftMenu">
+            <h2 title="Intersection of variables available for all the selected resources">
+                <img src="img/4step.png">
+                VARIABLE
+            </h2>
 
-        <div id="variableSelect"></div>
-		
+            <div id="variableSelect"></div>
+
         </div>
 
         <div class="leftMenu">
@@ -61,14 +61,14 @@
                 <img src="img/5step.png">
                 TIME PERIOD
             </h2>
-	
-	    <div id="timeSelect">
+
+            <div id="timeSelect">
                 <select id="timeYearSelect"></select>&nbsp;&nbsp;&nbsp;
                 <select id="timeMonthSelect"></select>
             </div>
         </div>
 
-	<div class="noticeLSCE leftMenu">
+        <div class="noticeLSCE leftMenu">
             Realised by <span title="Climate and Environment Sciences Laboratory" style="font-weight:bold;">LSCE</span> &nbsp;&nbsp;&nbsp; v1.1
         </div>
 
@@ -86,13 +86,13 @@
 
         <div class="rightMenuTool">
             Number of maps column<br>
-	    <select id="mapsNumberSelect" style="width: 60px;">
-		    <option value="1">1</option>
-		    <option value="2">2</option>
-		    <option value="3">3</option>
-		    <option value="4">4</option>
-		    <option value="5">5</option>
-	    </select>
+            <select id="mapsNumberSelect" style="width: 60px;">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
         </div>
 
         <div class="rightMenuTool">
@@ -100,19 +100,22 @@
                 <div id="submitDeleteMaps" class="rightMenuButton" title="Remove all maps">
                     <div class="lonelyToolIcone"><img src="img/fileclose.png"/></div>
                 </div>
-                <div id="submitShowAllLegends"  class="rightMenuButton" title="Show all legends">
+                <div id="submitShowAllLegends" class="rightMenuButton" title="Show all legends">
                     <div class="lonelyToolIcone"><img src="img/legend_display_all.png"/></div>
                 </div>
-                <div id="submitHideAllLegends"  class="rightMenuButton" title="Hide all legends">
+                <div id="submitHideAllLegends" class="rightMenuButton" title="Hide all legends">
                     <div class="lonelyToolIcone"><img src="img/legend_hide_all.png"/></div>
                 </div>
             </div>
         </div>
 
-        <div class="rightMenuTool"><HR width="50%" class="rightMenuHR"/></div>
+        <div class="rightMenuTool">
+            <HR width="50%" class="rightMenuHR"/>
+        </div>
 
         <div class="rightMenuTool">
             <h2>RANGE</h2>
+
             <div class="rightMenuSubTool">
                 <input id="getRange" type="button" title="Get range from variable" value="Get range"/>
                 <input type="text" id="slider-range-text" size="9" style="width: 80%; text-align: center;"/>
@@ -120,34 +123,40 @@
             <div id="slider-range" style="width: 80%;"></div>
         </div>
 
-        <div class="rightMenuTool"><HR width="50%" class="rightMenuHR"/></div>
+        <div class="rightMenuTool">
+            <HR width="50%" class="rightMenuHR"/>
+        </div>
 
         <div class="rightMenuTool">
             <h2>PALETTES</h2>
-	    <select id="paletteSelect" style="width: 80%;">
-		    <option value="alg">alg</option>
-		    <option value="alg2">alg2</option>
-		    <option value="blue_yellow_red">blue_yellow_red</option>
-		    <option value="ferret">ferret</option>
-		    <option value="greyscale">greyscale</option>
-		    <option value="ncview">ncview</option>
-		    <option value="occam">occam</option>
-		    <option value="occam_pastel-30">occam_pastel-30</option>
-		    <option value="redblue">redblue</option>
-		    <option value="sst_36">sst_36</option>
-		    <option value="green_magenta">green_magenta</option>
+            <select id="paletteSelect" style="width: 80%;">
+                <option value="alg">alg</option>
+                <option value="alg2">alg2</option>
+                <option value="blue_yellow_red">blue_yellow_red</option>
+                <option value="ferret">ferret</option>
+                <option value="greyscale">greyscale</option>
+                <option value="ncview">ncview</option>
+                <option value="occam">occam</option>
+                <option value="occam_pastel-30">occam_pastel-30</option>
+                <option value="redblue">redblue</option>
+                <option value="sst_36">sst_36</option>
+                <option value="green_magenta">green_magenta</option>
             </select>
         </div>
 
-        <div class="rightMenuTool">&nbsp;<HR width="50%" class="rightMenuHR"/></div>
+        <div class="rightMenuTool">&nbsp;
+            <HR width="50%" class="rightMenuHR"/>
+        </div>
 
         <div class="rightMenuTool">
             <h2>LEGEND</h2>
+
             <div class="rightMenuSubTool">
-            Colors number : <span id="slider-nbcolorbands-text"/>
-	    </div>
+                Colors number : <span id="slider-nbcolorbands-text"/>
+            </div>
             <div id="slider-nbcolorbands"></div>
-              <BR/><BR/>
+            <BR/><BR/>
+
             <div id="legend"></div>
         </div>
     </div>
@@ -162,41 +171,49 @@
 
 $properties = parse_ini_file( "bobcat.properties" );
 
-function fancytree_build_children ($dirtoread , $category, $elementToSelect) {
-     $files=glob($dirtoread."*.nc");
-     $len = count($files);
-     $counter = 0;
-     echo "\n";
-     foreach ($files as $file) {
-         if (is_file($file)) {
-             $counter++;
-             $bfile=basename($file);
-             $pfile=explode("_", $bfile);
-             // $pfile[1] represent the title, character "-" replaced by " "
-             $sfile=implode("_", array_slice($pfile, 0, 4));
-             $fileInfo = explode('.',$file);
-	     $fileInfo = $fileInfo[0].'.info';
-	     if (file_exists($fileInfo)) {
-             	$fileInfoContent = file_get_contents($fileInfo);
-                $fileInfoContent = str_replace("\n", "<br>", $fileInfoContent);
-                $fileInfoContent = str_replace("Ref :", "<b>Ref :</b>", $fileInfoContent);
-                $fileInfoContent = str_replace("Contact :", "<b>Contact :</b>", $fileInfoContent);
-	     } else
-		$fileInfoContent = "Not available";	 
-	     // If first element to be selected use next line and set true for elementToSelect argument
-             //$selectedElement = $elementToSelect && ($counter == 1) ? true : false;
-	     // To select a specific element
-             $selectedElement = $elementToSelect && strpos($sfile, $elementToSelect) ? true : false;
-             echo '                    {title:"'.str_replace("-", " ", $pfile[1]).'", key:"'.$sfile.'", selected: "'.$selectedElement.'", icon:false, url:"'.$category.'", complexToolTip:"'.$fileInfoContent.'",}';
-             if ( $counter != $len ) {
-                     echo ','."\n";
-             } else {
-                     // last line without ,
-                     echo "\n";
-             }
-         }
-     }
-     echo "\n";
+function fancytree_build_children( $dirtoread, $category, $elementToSelect )
+{
+    $files = glob( $dirtoread . "*.nc" );
+    $len = count( $files );
+    $counter = 0;
+    echo "\n";
+    foreach( $files as $file )
+    {
+        if( is_file( $file ) )
+        {
+            $counter++;
+            $bfile = basename( $file );
+            $pfile = explode( "_", $bfile );
+            // $pfile[1] represent the title, character "-" replaced by " "
+            $sfile = implode( "_", array_slice( $pfile, 0, 4 ) );
+            $fileInfo = explode( '.', $file );
+            $fileInfo = $fileInfo[0] . '.info';
+            if( file_exists( $fileInfo ) )
+            {
+                $fileInfoContent = file_get_contents( $fileInfo );
+                $fileInfoContent = str_replace( "\n", "<br>", $fileInfoContent );
+                $fileInfoContent = str_replace( "Ref :", "<b>Ref :</b>", $fileInfoContent );
+                $fileInfoContent = str_replace( "Contact :", "<b>Contact :</b>", $fileInfoContent );
+            }
+            else
+                $fileInfoContent = "Not available";
+            // If first element to be selected use next line and set true for elementToSelect argument
+            //$selectedElement = $elementToSelect && ($counter == 1) ? true : false;
+            // To select a specific element
+            $selectedElement = $elementToSelect && strpos( $sfile, $elementToSelect ) ? true : false;
+            echo '                    {title:"' . str_replace( "-", " ", $pfile[1] ) . '", key:"' . $sfile . '", selected: "' . $selectedElement . '", icon:false, url:"' . $category . '", complexToolTip:"' . $fileInfoContent . '",}';
+            if( $counter != $len )
+            {
+                echo ',' . "\n";
+            }
+            else
+            {
+                // last line without ,
+                echo "\n";
+            }
+        }
+    }
+    echo "\n";
 
 }
 
@@ -205,60 +222,62 @@ function fancytree_build_children ($dirtoread , $category, $elementToSelect) {
 <script type="text/javascript">
     $( document ).ready( function ()
     {
-	// Load properties file
-        jQuery.i18n.properties({
-                name:'bobcat',
-                path:'',
-                language:null,
-                mode:'both'
-        });
+        // Load properties file
+        jQuery.i18n.properties( {
+            name:'bobcat',
+            path:'',
+            language:null,
+            mode:'both'
+        } );
 
-	var resourcesTreeData = [
+        var resourcesTreeData = [
             {title:"Inversions", folder:true, expanded: false,
                 children: [
-                    <?php
-                    fancytree_build_children($properties["inversionsResourcesPath"], "Inversions", false);
-                    ?>
+<?php
+                    fancytree_build_children( $properties["inversionsResourcesPath"], "Inversions", false );
+                ?>
                 ]
             },
-	    {title:"Land Models", folder:true, expanded: true,
+            {title:"Land Models", folder:true, expanded: true,
                 children: [
-                    <?php
-                    fancytree_build_children($properties["landModelsResourcesPath"], "LandModels", "CLM4CN");
-                    ?>
+<?php
+                    fancytree_build_children( $properties["landModelsResourcesPath"], "LandModels", "CLM4CN" );
+                ?>
                 ]
             },
             {title:"Ocean Models", folder:true, expanded: false,
                 children: [
-                    <?php
-                    fancytree_build_children($properties["oceanModelsResourcesPath"], "OceanModels", false);
-                    ?>
+<?php
+                    fancytree_build_children( $properties["oceanModelsResourcesPath"], "OceanModels", false );
+                ?>
                 ]
             }
         ];
 
-	$("#projectionSelect").select2();
-	$("#projectionSelect").select2("val", "EPSG:4087");
+        $( "#projectionSelect" ).select2();
+        $( "#projectionSelect" ).select2( "val", "EPSG:4087" );
 
-	$("#periodSelect").select2();
-	$("#periodSelect").select2("val", "monthlymean");
+        $( "#periodSelect" ).select2();
+        $( "#periodSelect" ).select2( "val", "monthlymean" );
 
-	$("#mapsNumberSelect").select2();
-	$("#mapsNumberSelect").select2("val", "2");
+        $( "#mapsNumberSelect" ).select2();
+        $( "#mapsNumberSelect" ).select2( "val", "2" );
 
-	$("#paletteSelect").select2();
+        $( "#paletteSelect" ).select2();
 
-        function format(pal) {
-               return "<img class='flag' src='palettes/" + pal.text + ".png'/></br>" + pal.text;
+        function format( pal )
+        {
+            return "<img class='flag' src='palettes/" + pal.text + ".png'/></br>" + pal.text;
         }
-	$("#paletteSelect").select2({
-		formatResult: format
-	});
-	$("#paletteSelect").select2("val", "blue_yellow_red");
 
-	var variablesToKeepArray = ["Terrestrial_flux", "Ocean_flux"];
-	var variableNamesToKeepArray = ["Terrestrial flux", "Ocean flux"];
-	new BCInterfaceW( resourcesTreeData, variablesToKeepArray, variableNamesToKeepArray );
+        $( "#paletteSelect" ).select2( {
+            formatResult: format
+        } );
+        $( "#paletteSelect" ).select2( "val", "blue_yellow_red" );
+
+        var variablesToKeepArray = ["Terrestrial_flux", "Ocean_flux"];
+        var variableNamesToKeepArray = ["Terrestrial flux", "Ocean flux"];
+        new BCInterfaceW( resourcesTreeData, variablesToKeepArray, variableNamesToKeepArray );
 
     } );
 </script>
